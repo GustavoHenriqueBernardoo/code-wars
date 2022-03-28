2cd8c9b6cc3bc5072506ce7459c68303229c4306
Gustavo Henrique Bernardo <gustavobernardo094@gmail.com>
2022-03-28 12:08:01 -0300

    Personalized Message and Calculate average

--- app.js
+++ app.js
@@ -20,8 +20,51 @@ function findSmallestInt(args) {
       smallest = currentNum
     }
   }
-  console.log(smallest)
   return smallest
+
+  // return Math.min(...arg) The best Practices
 }
 
 findSmallestInt([78, 56, 232, 12, 8])
+
+
+// Personalized Message
+
+/*
+Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+
+Use conditionals to return the proper message:
+
+case	return
+name equals owner	'Hello boss'
+otherwise	'Hello guest'
+
+*/
+
+function greet(name, owner) {
+  return name === owner ? 'Hello boss' : 'Hello guest'
+}
+
+
+// Calculate average
+
+/*
+Write a function which calculates the average of the numbers in a given list.
+
+Note: Empty arrays should return 0.
+*/
+
+function find_average(array) {
+  // your code here
+  if (array.length === 0) {
+    return 0
+  }
+  let total = 0
+  for (let i = 0; i < array.length; i++) {
+    let currentNum = array[i]
+    total += currentNum
+  }
+  return total / array.length
+}
+
+console.log(find_average([]))
